7df037fb95f28f580389f3db07f4a54287480720
koyelucd <koyel.majumdar@ucdconnect.ie>
2023-09-09 12:03:57 +0100

    Commit message

--- R/plot_thresholds.R
+++ R/plot_thresholds.R
@@ -10,6 +10,8 @@
 #' \code{\link[betaHMM:betaHMMResults]{betaHMMResults}}/
 #' \code{\link[betaHMM:dmcResults]{dmcResults}}/
 #' \code{\link[betaHMM:dmcResults]{threshold_Results}} object.
+#' @param plot_threshold The "TRUE" option displays the threshold points in the
+#' graph for the 3 state betaHMM model (default = "FALSE").
 #' @param title The title that the user wants to display.
 #' If no title is to be displayed the default is 'NULL'.
 #' @param ... Other graphics parameters.
@@ -20,21 +22,21 @@
 #' @importFrom ggplot2 ggplot aes
 #' @importFrom scales seq_gradient_pal
 setMethod(f = "plot", signature(x = "threshold_Results"),
-            definition = function(x, title = NULL, ...) {
+            definition = function(x, plot_threshold=TRUE,title = NULL, ...) {
     # x <- object
     graph_objects <- c()
 
     object <- x
-    graph_objects <- thresholdGlobalplots(object, title = title, ...)
+    graph_objects <- thresholdGlobalplots(object,plot_threshold=plot_threshold,
+                                            title = title, ...)
     return(graph_objects)
 })
 
-thresholdGlobalplots <- function(x, title = NULL, ...) {
+thresholdGlobalplots <- function(x, plot_threshold=TRUE,title = NULL, ...) {
     txt <- ifelse(is.null(title), "", title)
     data <- as.data.frame(annotatedData(x))
     df_plot <- subset(data, select = -c(IlmnID))
-    data_x <- sort(df_plot[, 1])
-    K <- K(x)
+    data_x <- sort(df_plot[, 1]); K <- K(x)
     prop <- as.numeric(table(hidden_states(x)))/nrow(data)
     data_th_plot <- matrix(data = NA, nrow = 1, ncol = 3)
     data_th_plot <- as.data.frame(data_th_plot)
@@ -61,20 +63,20 @@ thresholdGlobalplots <- function(x, title = NULL, ...) {
     p.data <- ggplot_build(plot_graph)$data[[1]]
     p.text <- lapply(split(p.data, f = p.data$group), function(df) {
         df[which.max(df$y), ]})
-    p.text <- do.call(rbind, p.text)
-    p.text$prop <- prop
+    p.text <- do.call(rbind, p.text); p.text$prop <- prop
     plot_graph <- plot_graph + annotate("text", x = p.text$x, y = 0.2,
                                         label = sprintf("%.3f", p.text$prop),
                                         colour = p.text$colour, vjust = 0)
-
-
-    if (!is.null(plot_graph)) {
-        th_plot <- threshold(x)$thresholds
-        ano_th <- th_plot - 0.02 }
-    num <- sort(p.text$y)
-    ano_y <- num[length(num)] - 0.1
-    plot_graph <- plot_graph + geom_vline(xintercept = th_plot,
-                                            linetype = "dotted") +
-        annotate("text", x = ano_th, y = ano_y,
-        label = th_plot, angle = 90)
+    if(plot_threshold==TRUE)
+    {
+        if (!is.null(plot_graph)) {
+            th_plot <- as.vector(unlist(threshold(x)))
+            ano_th <- th_plot - 0.02 }
+        num <- sort(p.text$y)
+        ano_y <- num[length(num)] - 0.1
+        plot_graph <- plot_graph + geom_vline(xintercept = th_plot,
+                                              linetype = "dotted") +
+            annotate("text", x = ano_th, y = ano_y,
+                     label = th_plot, angle = 90)
+    }
     return(plot_graph)}
